41174664ac0ae82c9ea8fe8de5acf6307afd813d
agu22604 <agu22604@uvg.edu.gt>
2024-08-26 19:34:10 -0600

    Create config.h

---
+++ config.h
@@ -0,0 +1,14 @@
+
+//Adafruit IO config
+#define IO_USERNAME  "joseaguilaaar"
+#define IO_KEY       "aio_Eywn88JHVmyy1nj2kNBE6LiJFMm3"
+//WIFI 
+#define WIFI_SSID "iPhone122"
+#define WIFI_PASS "digital2"
+
+#include "AdafruitIO_WiFi.h"
+
+#
+AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
+
+
